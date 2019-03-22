61ccb8b7c2f0c5718cb1e3c2ae53a8ea2aee8912
JustBeLeo <sdk1373@sina.com>
2019-03-22 18:08:01 +0800

    add ViewPager

---
+++ app/src/main/java/com/android/sdk13/mobileplayer/Pager/Adapter/MySpacePagerAdpter.java
@@ -0,0 +1,35 @@
+package com.android.sdk13.mobileplayer.Pager.Adapter;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.android.sdk13.mobileplayer.Pager.Fragment.MySpaceFragment;
+
+import java.util.ArrayList;
+
+public class MySpacePagerAdpter extends FragmentPagerAdapter {
+
+    ArrayList<MySpaceFragment> list;
+    public MySpacePagerAdpter(FragmentManager fm,ArrayList<MySpaceFragment> list) {
+        super( fm );
+        this.list = list;
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return list.get( i );
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return list.get( position ).getTitle();
+    }
+}
